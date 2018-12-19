84e578b27469dab6a13be06c50baf39921c6b048
ouki <358023925@qq.com>
2018-12-19 17:23:49 +0800

    Update maqueen.ts

--- maqueen.ts
+++ maqueen.ts
@@ -128,7 +128,7 @@ namespace maqueen{
 
         switch (unit) {
             case PingUnit.Centimeters: return d / 42;
-            default: return <number>d ;
+            default: return parseInt(d) ;
         }
     }
     
