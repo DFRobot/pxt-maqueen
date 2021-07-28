20c8aad12b6e69ce52f213a17cd3d02e5ab6d4a3
TgJe <51323906+tangjie133@users.noreply.github.com>
2021-07-28 13:36:04 +0800

    更新 pxt.json, maqueen.ts

--- maqueen.ts
+++ maqueen.ts
@@ -183,32 +183,33 @@ namespace maqueen {
 
 function valuotokeyConversion():number{
     let irdata:number;
-    switch(irCode()){
-        case 0xff00:irdata = 0;break;
-        case 0xfe01:irdata = 1;break;
-        case 0xfd02:irdata = 2;break;
-        case 0xfb04:irdata = 4;break;
-        case 0xfa05:irdata = 5;break;
-        case 0xf906:irdata = 6;break;
-        case 0xf708:irdata = 8;break;
-        case 0xf609:irdata = 9;break;
-        case 0xf50a:irdata = 10;break;
-        case 0xf30c:irdata = 12;break;
-        case 0xf20d:irdata = 13;break;
-        case 0xf10e:irdata = 14;break;
-        case 0xef10:irdata = 16;break;
-        case 0xee11:irdata = 17;break;
-        case 0xed12:irdata = 18;break;
-        case 0xeb14:irdata = 20;break;
-        case 0xea15:irdata = 21;break;
-        case 0xe916:irdata = 22;break;
-        case 0xe718:irdata = 24;break;
-        case 0xe619:irdata = 25;break;
-        case 0xe51a:irdata = 20;break;
-        default:
-         irdata = -1;
-    }
-    return irdata;
+    // switch(irCode()){
+    //     case 0xff00:irdata = 0;break;
+    //     case 0xfe01:irdata = 1;break;
+    //     case 0xfd02:irdata = 2;break;
+    //     case 0xfb04:irdata = 4;break;
+    //     case 0xfa05:irdata = 5;break;
+    //     case 0xf906:irdata = 6;break;
+    //     case 0xf708:irdata = 8;break;
+    //     case 0xf609:irdata = 9;break;
+    //     case 0xf50a:irdata = 10;break;
+    //     case 0xf30c:irdata = 12;break;
+    //     case 0xf20d:irdata = 13;break;
+    //     case 0xf10e:irdata = 14;break;
+    //     case 0xef10:irdata = 16;break;
+    //     case 0xee11:irdata = 17;break;
+    //     case 0xed12:irdata = 18;break;
+    //     case 0xeb14:irdata = 20;break;
+    //     case 0xea15:irdata = 21;break;
+    //     case 0xe916:irdata = 22;break;
+    //     case 0xe718:irdata = 24;break;
+    //     case 0xe619:irdata = 25;break;
+    //     case 0xe51a:irdata = 20;break;
+    //     default:
+    //      irdata = -1;
+    // }
+    irdata = irCode();
+    return (irdata & 0XFF);
 }
 
     basic.forever(() => {
